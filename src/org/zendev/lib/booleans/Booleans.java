e56704aee5b65018b49e4347992e55e16d90565d
Mehdi Lavasani <mehdilavasani79@gmail.com>
2024-04-04 15:44:39 +0330

    Boolean data type utilities

---
+++ src/org/zendev/lib/booleans/Booleans.java
@@ -0,0 +1,18 @@
+package org.zendev.lib.booleans;
+
+public class Booleans {
+
+    public int booleanToNumber(boolean flag) {
+        return flag ? 1 : 0;
+    }
+
+    public int[] booleansToNumbers(boolean... array) {
+        int[] numbers = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            numbers[i] = array[i] ? 1 : 0;
+        }
+
+        return numbers;
+    }
+}
